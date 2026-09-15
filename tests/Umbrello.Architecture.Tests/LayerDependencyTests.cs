dfaf8c53a035e3e367b2791320d19fe7c59296aa
luispfatdo <luis.paulo.tdo@gmail.com>
2026-09-15 19:15:37 -0300

    Declarando os assemblies dos projetos que serão testados no LayerDependencyTests.

--- tests/Umbrello.Architecture.Tests/LayerDependencyTests.cs
+++ tests/Umbrello.Architecture.Tests/LayerDependencyTests.cs
@@ -1,6 +1,12 @@
 using System.Reflection;
 
+namespace Umbrello.Architecture.Tests;
+
 public class LayerDependencyTests
 {
     private static readonly Assembly Domain = typeof(Umbrello.Domain.AssemblyReference).Assembly;
+    private static readonly Assembly Contracts = typeof(Umbrello.Contracts.AssemblyReference).Assembly;
+    private static readonly Assembly Application = typeof(Umbrello.Application.AssemblyReference).Assembly;
+    private static readonly Assembly Infrastructure = typeof(Umbrello.Infrastructure.AssemblyReference).Assembly;
+    private static readonly Assembly Messaging = typeof(Umbrello.Messaging.RabbitMq.AssemblyReference).Assembly;
 }
